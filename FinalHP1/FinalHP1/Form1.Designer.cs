4f70e7bcf1737021d0c32489edece0f15e021d87
JuanEchavarriaC <juan.echavarria517@pascualbravo.edu.co>
2024-11-14 18:24:10 -0500

    Avance de botones izq de añadir editar y codigo de la primer opcion de material

--- FinalHP1/FinalHP1/Form1.Designer.cs
+++ FinalHP1/FinalHP1/Form1.Designer.cs
@@ -28,34 +28,180 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnRegistrarMaterial = new System.Windows.Forms.Button();
+            this.btnRegistrarPersona = new System.Windows.Forms.Button();
+            this.btnEliminarPersona = new System.Windows.Forms.Button();
+            this.btnRegistrarPrestamo = new System.Windows.Forms.Button();
+            this.btnRegistrarDevolucion = new System.Windows.Forms.Button();
+            this.btnIncremenarCantidad = new System.Windows.Forms.Button();
+            this.btnConsultarHistorial = new System.Windows.Forms.Button();
+            this.textBoxMaterialId = new System.Windows.Forms.TextBox();
+            this.textBoxTitulo = new System.Windows.Forms.TextBox();
+            this.dateTimePickerFechaRegistro = new System.Windows.Forms.DateTimePicker();
+            this.numericUpDownCantidad = new System.Windows.Forms.NumericUpDown();
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
+            this.textBoxCedula = new System.Windows.Forms.TextBox();
+            this.comboBoxRol = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCantidad)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // btnRegistrarMaterial
             // 
-            this.button1.Location = new System.Drawing.Point(264, 160);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnRegistrarMaterial.Location = new System.Drawing.Point(37, 36);
+            this.btnRegistrarMaterial.Name = "btnRegistrarMaterial";
+            this.btnRegistrarMaterial.Size = new System.Drawing.Size(155, 36);
+            this.btnRegistrarMaterial.TabIndex = 0;
+            this.btnRegistrarMaterial.Text = "Registrar material";
+            this.btnRegistrarMaterial.UseVisualStyleBackColor = true;
+            this.btnRegistrarMaterial.Click += new System.EventHandler(this.btnRegistrarMaterial_Click);
+            // 
+            // btnRegistrarPersona
+            // 
+            this.btnRegistrarPersona.Location = new System.Drawing.Point(37, 78);
+            this.btnRegistrarPersona.Name = "btnRegistrarPersona";
+            this.btnRegistrarPersona.Size = new System.Drawing.Size(155, 38);
+            this.btnRegistrarPersona.TabIndex = 1;
+            this.btnRegistrarPersona.Text = "Registrar persona";
+            this.btnRegistrarPersona.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminarPersona
+            // 
+            this.btnEliminarPersona.Location = new System.Drawing.Point(37, 122);
+            this.btnEliminarPersona.Name = "btnEliminarPersona";
+            this.btnEliminarPersona.Size = new System.Drawing.Size(155, 38);
+            this.btnEliminarPersona.TabIndex = 2;
+            this.btnEliminarPersona.Text = "Eliminar persona";
+            this.btnEliminarPersona.UseVisualStyleBackColor = true;
+            // 
+            // btnRegistrarPrestamo
+            // 
+            this.btnRegistrarPrestamo.Location = new System.Drawing.Point(37, 166);
+            this.btnRegistrarPrestamo.Name = "btnRegistrarPrestamo";
+            this.btnRegistrarPrestamo.Size = new System.Drawing.Size(155, 38);
+            this.btnRegistrarPrestamo.TabIndex = 3;
+            this.btnRegistrarPrestamo.Text = "Registrar prestamo";
+            this.btnRegistrarPrestamo.UseVisualStyleBackColor = true;
+            // 
+            // btnRegistrarDevolucion
+            // 
+            this.btnRegistrarDevolucion.Location = new System.Drawing.Point(37, 210);
+            this.btnRegistrarDevolucion.Name = "btnRegistrarDevolucion";
+            this.btnRegistrarDevolucion.Size = new System.Drawing.Size(155, 38);
+            this.btnRegistrarDevolucion.TabIndex = 4;
+            this.btnRegistrarDevolucion.Text = "Registrar devolucion";
+            this.btnRegistrarDevolucion.UseVisualStyleBackColor = true;
+            // 
+            // btnIncremenarCantidad
+            // 
+            this.btnIncremenarCantidad.Location = new System.Drawing.Point(37, 254);
+            this.btnIncremenarCantidad.Name = "btnIncremenarCantidad";
+            this.btnIncremenarCantidad.Size = new System.Drawing.Size(155, 38);
+            this.btnIncremenarCantidad.TabIndex = 5;
+            this.btnIncremenarCantidad.Text = "Incrementar cantidad";
+            this.btnIncremenarCantidad.UseVisualStyleBackColor = true;
+            // 
+            // btnConsultarHistorial
+            // 
+            this.btnConsultarHistorial.Location = new System.Drawing.Point(37, 298);
+            this.btnConsultarHistorial.Name = "btnConsultarHistorial";
+            this.btnConsultarHistorial.Size = new System.Drawing.Size(155, 38);
+            this.btnConsultarHistorial.TabIndex = 6;
+            this.btnConsultarHistorial.Text = "Consultar historial";
+            this.btnConsultarHistorial.UseVisualStyleBackColor = true;
+            // 
+            // textBoxMaterialId
+            // 
+            this.textBoxMaterialId.Location = new System.Drawing.Point(290, 42);
+            this.textBoxMaterialId.Name = "textBoxMaterialId";
+            this.textBoxMaterialId.Size = new System.Drawing.Size(100, 22);
+            this.textBoxMaterialId.TabIndex = 7;
+            // 
+            // textBoxTitulo
+            // 
+            this.textBoxTitulo.Location = new System.Drawing.Point(414, 43);
+            this.textBoxTitulo.Name = "textBoxTitulo";
+            this.textBoxTitulo.Size = new System.Drawing.Size(100, 22);
+            this.textBoxTitulo.TabIndex = 8;
+            // 
+            // dateTimePickerFechaRegistro
+            // 
+            this.dateTimePickerFechaRegistro.Location = new System.Drawing.Point(546, 43);
+            this.dateTimePickerFechaRegistro.Name = "dateTimePickerFechaRegistro";
+            this.dateTimePickerFechaRegistro.Size = new System.Drawing.Size(285, 22);
+            this.dateTimePickerFechaRegistro.TabIndex = 9;
+            // 
+            // numericUpDownCantidad
+            // 
+            this.numericUpDownCantidad.Location = new System.Drawing.Point(874, 42);
+            this.numericUpDownCantidad.Name = "numericUpDownCantidad";
+            this.numericUpDownCantidad.Size = new System.Drawing.Size(61, 22);
+            this.numericUpDownCantidad.TabIndex = 10;
+            // 
+            // textBoxNombre
+            // 
+            this.textBoxNombre.Location = new System.Drawing.Point(290, 86);
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(100, 22);
+            this.textBoxNombre.TabIndex = 11;
+            // 
+            // textBoxCedula
+            // 
+            this.textBoxCedula.Location = new System.Drawing.Point(414, 86);
+            this.textBoxCedula.Name = "textBoxCedula";
+            this.textBoxCedula.Size = new System.Drawing.Size(100, 22);
+            this.textBoxCedula.TabIndex = 12;
+            // 
+            // comboBoxRol
+            // 
+            this.comboBoxRol.FormattingEnabled = true;
+            this.comboBoxRol.Location = new System.Drawing.Point(546, 83);
+            this.comboBoxRol.Name = "comboBoxRol";
+            this.comboBoxRol.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxRol.TabIndex = 13;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1002, 580);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBoxRol);
+            this.Controls.Add(this.textBoxCedula);
+            this.Controls.Add(this.textBoxNombre);
+            this.Controls.Add(this.numericUpDownCantidad);
+            this.Controls.Add(this.dateTimePickerFechaRegistro);
+            this.Controls.Add(this.textBoxTitulo);
+            this.Controls.Add(this.textBoxMaterialId);
+            this.Controls.Add(this.btnConsultarHistorial);
+            this.Controls.Add(this.btnIncremenarCantidad);
+            this.Controls.Add(this.btnRegistrarDevolucion);
+            this.Controls.Add(this.btnRegistrarPrestamo);
+            this.Controls.Add(this.btnEliminarPersona);
+            this.Controls.Add(this.btnRegistrarPersona);
+            this.Controls.Add(this.btnRegistrarMaterial);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCantidad)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnRegistrarMaterial;
+        private System.Windows.Forms.Button btnRegistrarPersona;
+        private System.Windows.Forms.Button btnEliminarPersona;
+        private System.Windows.Forms.Button btnRegistrarPrestamo;
+        private System.Windows.Forms.Button btnRegistrarDevolucion;
+        private System.Windows.Forms.Button btnIncremenarCantidad;
+        private System.Windows.Forms.Button btnConsultarHistorial;
+        private System.Windows.Forms.TextBox textBoxMaterialId;
+        private System.Windows.Forms.TextBox textBoxTitulo;
+        private System.Windows.Forms.DateTimePicker dateTimePickerFechaRegistro;
+        private System.Windows.Forms.NumericUpDown numericUpDownCantidad;
+        private System.Windows.Forms.TextBox textBoxNombre;
+        private System.Windows.Forms.TextBox textBoxCedula;
+        private System.Windows.Forms.ComboBox comboBoxRol;
     }
 }
 
--- FinalHP1/FinalHP1/Form1.cs
+++ FinalHP1/FinalHP1/Form1.cs
@@ -21,10 +21,76 @@ namespace FinalHP1
             biblioteca = new Biblioteca(); // Instancia la lógica de biblioteca
         }
 
-
-        private void button1_Click(object sender, EventArgs e)
+        private void btnRegistrarMaterial_Click(object sender, EventArgs e)
         {
+            try
+        {
+        // Obtener los valores de los campos de entrada
+        string id = textBoxMaterialId.Text;
+        string titulo = textBoxTitulo.Text;
+        DateTime fechaRegistro = dateTimePickerFechaRegistro.Value;
+        int cantidadRegistrada = (int)numericUpDownCantidad.Value;
+
+        // Validar que los campos necesarios no estén vacíos
+        if (string.IsNullOrWhiteSpace(id) || string.IsNullOrWhiteSpace(titulo))
+        {
+            MessageBox.Show("Por favor, complete todos los campos obligatorios.", "Error");
+            return;
+        }
+
+        // Registrar el material en la biblioteca
+        biblioteca.RegistrarMaterial(id, titulo, fechaRegistro, cantidadRegistrada);
+        
+        // Confirmación de éxito
+        MessageBox.Show("Material registrado exitosamente.", "Éxito");
+
+        // Limpiar los campos de entrada
+        textBoxMaterialId.Clear();
+        textBoxTitulo.Clear();
+        dateTimePickerFechaRegistro.Value = DateTime.Now;
+        numericUpDownCantidad.Value = 1;
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Error al registrar el material: {ex.Message}", "Error");
+        }
 
         }
+
+
+        private void btnRegistrarPersona_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Obtener los valores de los campos de entrada
+                string nombre = textBoxNombre.Text;
+                string cedula = textBoxCedula.Text;
+                string rol = comboBoxRol.SelectedItem?.ToString(); // Verifica que se haya seleccionado un rol
+
+                // Validar que los campos no estén vacíos
+                if (string.IsNullOrWhiteSpace(nombre) || string.IsNullOrWhiteSpace(cedula) || string.IsNullOrWhiteSpace(rol))
+                {
+                    MessageBox.Show("Por favor, complete todos los campos obligatorios.", "Error");
+                    return;
+                }
+
+                // Registrar la persona en la biblioteca
+                biblioteca.RegistrarPersona(nombre, cedula, rol);
+
+                // Confirmación de éxito
+                MessageBox.Show("Persona registrada exitosamente.", "Éxito");
+
+                // Limpiar los campos de entrada
+                textBoxNombre.Clear();
+                textBoxCedula.Clear();
+                comboBoxRol.SelectedIndex = -1; // Restablecer el comboBox
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al registrar la persona: {ex.Message}", "Error");
+            }
+        }
+
+
     }
 }
